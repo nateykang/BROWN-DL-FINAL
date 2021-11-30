09332c274cf1a3ba584a61487b7db89485a3d0db
emmamcfall <emma_mcfall@brown.edu>
2021-11-30 17:49:27 -0500

    testing

--- assignment.py
+++ assignment.py
@@ -1,9 +1,61 @@
-#assignment
 
-import csv
-import numpy as np
 import tensorflow as tf
+import numpy as np
 
-with open('ex_file.csv','r') as ex_file:
-    data = csv.reader(ex_file, delimiter = ',')
-data_array = np.asarray(data)
+class Model(tf.keras.Model):
+    def __init__(self):
+        """
+        NEED TO EDIT THIS
+        """
+        super(Model, self).__init__()
+
+        self.batch_size = 100
+        self.num_teams = # Need to fill this in
+        self.learning_rate = tf.keras.optimizers.Adam(.001)
+
+
+    def call(self, inputs):
+        """
+        NEED TO FILL IN
+        """
+
+        pass
+
+    def loss(self, logits, labels):
+        """
+        NEED TO FILL IN
+        """
+        pass
+
+    def accuracy(self, logits, labels):
+        """
+        NEED TO FILL IN
+        """
+        pass
+
+
+def train(model, train_inputs, train_labels):
+    '''
+    NEED TO FILL IN
+    '''
+
+    pass
+
+
+def test(model, test_inputs, test_labels):
+    """
+    NEED TO FILL IN
+    """
+    pass
+
+
+def main():
+    '''
+    NEED TO FILL IN
+    '''
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
