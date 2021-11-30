51f7d261139dea83a5c65ce20017184baa363f5e
emmamcfall <emma_mcfall@brown.edu>
2021-11-30 17:09:09 -0500

    test

--- assignment.py
+++ assignment.py
@@ -1,1 +1,9 @@
 #assignment
+
+import csv
+import numpy as np
+import tensorflow as tf
+
+with open('ex_file.csv','r') as ex_file:
+    data = csv.reader(ex_file, delimiter = ',')
+data_array = np.asarray(data)
