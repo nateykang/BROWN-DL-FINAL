6bd22ff764be4a812e21431182849d2927332b31
emmamcfall <emma_mcfall@brown.edu>
2021-12-04 11:27:34 -0500

    still working on fixing some preprocessing

--- assignment.py
+++ assignment.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import numpy as np
 from preprocessing import get_data
 from preprocessing import get_labels
+from preprocessing import get_next_batch
 
 class Model(tf.keras.Model):
     def __init__(self):
@@ -11,23 +12,34 @@ class Model(tf.keras.Model):
         """
         super(Model, self).__init__()
 
-        self.batch_size = 100
-        self.num_teams = 127
+        self.batch_size = 18
+        self.num_teams = 126
         self.learning_rate = tf.keras.optimizers.Adam(.001)
+        self.num_epochs = 10
+        self.hidden_size = 100
+
+        self.layer1 = tf.keras.layers.Dense(self.hidden_size,activation='relu')
+        self.layer2 = tf.keras.layers.Dense(self.hidden_size, activation='relu')
+        self.layer3 = tf.keras.layers.Dense(1)
 
 
     def call(self, inputs):
         """
-        NEED TO FILL IN
+        Completed the forward pass through the network, obtaining logits
+        :param inputs: shape of (num_teams, 27)
+        :return: logits - a matrix of shape (num_inputs, 1)
         """
-
-        pass
+        return self.layer3(self.layer2(self.layer1(inputs)))
 
     def loss(self, logits, labels):
         """
-        NEED TO FILL IN
+        Here we calculate loss by comparing the logits, calculated in the call function with the labels
+        :param logits: shape of (num_teams, 1)
+        :param labels: shape of (num_teams, 1)
+        :return: loss - a Tensor with a single entry
         """
-        pass
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels, logits))
+        return loss
 
     def accuracy(self, logits, labels):
         """
@@ -38,8 +50,27 @@ class Model(tf.keras.Model):
 
 def train(model, train_inputs, train_labels):
     '''
-    NEED TO FILL IN
+    This function is where we complete the training of our model, using training inputs and training labels.
+    Batch the inputs, and complete the forward pass to obtain probabilities.
+    Then we complete gradient descent and use the loss to update trainable variables correctly
+    :param model: the initialized model to use for the forward
+    pass and backward pass
+    :param train_inputs: train inputs (all inputs to use for training), dim of [num_teams, 27]
+    :param train_labels: train labels (all labels to use for training), dim of [num_teams, 1]
+    :return: None
     '''
+
+    # Need to shuffle inputs and labels here using index
+
+    for i in range(model.batch_size):
+        batched_inputs, batched_labels = get_next_batch(train_inputs, train_labels, model.batch_size, i)
+        probs = model.call(batched_inputs.astype(np.float))
+
+        with tf.GradientTape() as tape:
+            batch_loss = model.loss(probs, batched_labels)
+            gradients = tape.gradient(batch_loss, model.trainable_variables)
+            model.learning_rate.apply_gradients(zip(gradients, model.trainable_variables))
+        print(i)
 
     pass
 
@@ -53,17 +84,10 @@ def test(model, test_inputs, test_labels):
 
 def main():
     '''
-    NEED TO FILL IN
+    This function is where we initialize our model, call our functions from preprocessing to obtain data and labels
+    , and run training and testing functions to obtain a numerical level of accuracy.
+    :return: None
     '''
-    #train_data = get_data(r"C:\Users\natha\OneDrive\Documents\GitHub\cs1470\BROWN-DL-FINAL-EM_NK\data\recruiting_rankings_2014.csv",
-    #r"C:\Users\natha\OneDrive\Documents\GitHub\cs1470\BROWN-DL-FINAL-EM_NK\data\recruiting_rankings_2015.csv",
-    #r"C:\Users\natha\OneDrive\Documents\GitHub\cs1470\BROWN-DL-FINAL-EM_NK\data\recruiting_rankings_2016.csv",
-    #r"C:\Users\natha\OneDrive\Documents\GitHub\cs1470\BROWN-DL-FINAL-EM_NK\data\recruiting_rankings_2017.csv",
-    #r"C:\Users\natha\OneDrive\Documents\GitHub\cs1470\BROWN-DL-FINAL-EM_NK\data\team_records_2014.csv",
-    #r"C:\Users\natha\OneDrive\Documents\GitHub\cs1470\BROWN-DL-FINAL-EM_NK\data\team_records_2015.csv",
-    #r"C:\Users\natha\OneDrive\Documents\GitHub\cs1470\BROWN-DL-FINAL-EM_NK\data\team_records_2016.csv",
-    #r"C:\Users\natha\OneDrive\Documents\GitHub\cs1470\BROWN-DL-FINAL-EM_NK\data\team_records_2017.csv",
-    #r"C:\Users\natha\OneDrive\Documents\GitHub\cs1470\BROWN-DL-FINAL-EM_NK\data\returning_production_2018.csv",)
 
     train_and_test_data = get_data(
         'data/recruiting_rankings_2014.csv',
@@ -76,10 +100,33 @@ def main():
         'data/team_records_2017.csv',
         'data/returning_production_2018.csv')
 
-    train_labels = get_labels('data/expected_wins_2018.csv','data/team_talent_2018.csv','data/predicted_points_added_2018.csv',2018)
+    train_labels = (get_labels('data/expected_wins_2018.csv','data/team_talent_2018.csv','data/predicted_points_added_2018.csv',2018))
     test_labels = get_labels('data/expected_wins_2019.csv','data/team_talent_2019.csv','data/predicted_points_added_2019.csv',2019)
 
+    # Making sure we only include teams that are in all three sets
+    #print(train_and_test_data.columns)
+    #print(train_labels.columns)
+    train_labels = train_labels[train_labels['team'].isin(train_and_test_data['team'])]
+    train_and_test_data = train_and_test_data[train_and_test_data['team'].isin(train_labels['team'])]
+    test_labels = test_labels[test_labels['team'].isin(train_and_test_data['team'])]
 
+    # Preprocessing training labels to be a [num_examples, 1] shape
+    conference_list = (train_labels['conference'].unique())
+    train_labels = train_labels.replace(conference_list, list(range(len(conference_list))))
+    processed_labels = np.asarray(train_labels)
+    processed_train_labels = np.prod(np.delete(processed_labels, 0, 1), 1)
+
+    # Preprocessing test labels to be a [num_examples, 1] shape
+    conference_list = (test_labels['conference'].unique())
+    test_labels = test_labels.replace(conference_list, list(range(len(conference_list))))
+    processed_labels = np.asarray(test_labels)
+    processed_test_labels = np.prod(np.delete(processed_labels, 0, 1), 1)
+
+    # For now, using the same data for training and testing, just different labels
+    # We can change this later, just wanted to set up forward network
+    model = Model()
+    #for i in range(model.num_epochs):
+    train(model, np.asarray(train_and_test_data), processed_train_labels)
 
 if __name__ == '__main__':
     main()
--- preprocessing.py
+++ preprocessing.py
@@ -19,13 +19,15 @@ def get_data(rr_y1,rr_y2,rr_y3,rr_y4,tr_y1,tr_y2,tr_y3,tr_y4,rp):
     df_tr4 = pd.read_csv(tr_y4)
     df_rp = pd.read_csv(rp).drop(columns='season')
     data_frames = [df_rr1,df_rr2,df_rr3,df_rr4,df_tr1,df_tr2,df_tr3,df_tr4,df_rp]
-    #df_merged_r = functools.reduce(lambda  left,right: pd.merge(left,right,on=['team'],how='inner'), data_frames)
+    df_merged_r = functools.reduce(lambda  left,right: pd.merge(left,right,on=['team'],how='inner'), data_frames)
     #k = np.arange(len(data_frames)).astype(str)
-    df_merged_c = pd.concat([x.set_index('team') for x in data_frames], axis=1, join='inner')
-    print(df_merged_c.head)
+    #df_merged_c = pd.concat([x.set_index('team') for x in data_frames], axis=1, join='inner')
+    print(df_merged_r.head)
+    #df_merged_r.to_csv('data/merged_concat_no_year_with_teams.csv', header=True)
     #df_merged_r.to_csv(r'C:\Users\natha\OneDrive\Documents\GitHub\cs1470\BROWN-DL-FINAL-EM_NK\data\merged_reduce.csv',header=True)
     #df_merged_c.to_csv(r'C:\Users\natha\OneDrive\Documents\GitHub\cs1470\BROWN-DL-FINAL-EM_NK\data\merged_concat_no_year.csv',header=True)
-    return df_merged_c
+    #df_merged_c.to_csv('data/merged_concat_no_year.csv',header=True)
+    return df_merged_r
     pass
 
 def get_labels(exp_wins, tt, ppa, year):
@@ -38,11 +40,17 @@ def get_labels(exp_wins, tt, ppa, year):
     df_ppa_1 = pd.read_csv(ppa).drop(columns='conference')
     data_frames = [df_exp_wins_1, df_tt_1,df_ppa_1]
     df_merged_r = functools.reduce(lambda left, right: pd.merge(left, right, on=['team'], how='inner'), data_frames)
-    print(df_merged_r.head)
-    if year == 2019:
-        df_merged_r.to_csv('data/labels_test_merged_concat.csv',header=True)
-    else:
-        df_merged_r.to_csv('data/labels_train_merged_concat.csv', header=True)
+    #print(df_merged_r.head)
+    #if year == 2019:
+        #df_merged_r.to_csv('data/labels_test_merged_concat.csv',header=True)
+    #else:
+        #df_merged_r.to_csv('data/labels_train_merged_concat.csv', header=True)
     return df_merged_r
+
+def get_next_batch(inputs, labels, batch_size, i ):
+    # a helper function for the model where we can batch our data and labels appropriately
+    batched_inputs = inputs[i*batch_size:batch_size][:]
+    batched_labels = labels[i*batch_size:batch_size]
+    return batched_inputs, batched_labels
 
 
